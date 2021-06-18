bb129f07ae52b4e09ddd21f19d704e6453b5b339
sujy <sujy@akulaku.com>
2021-06-18 15:52:54 +0800

    del demo for mybatis-plus

--- luwu-dao/src/main/java/com/github/backbook/luwu/dao/mapper/EmployeeMapper.java
+++
@@ -1,7 +0,0 @@
-package com.github.backbook.luwu.dao.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.github.backbook.luwu.model.pojo.Employee;
-
-public interface EmployeeMapper extends BaseMapper<Employee> {
-}
